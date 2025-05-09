c2629aa2f939c4e6019ec4152c45025ce95bb05f
AntoPrince-502206 <anto.502206@sxcce.edu.in>
2025-05-09 17:03:24 +0530

    Add files via upload

---
+++ mavenproject1/src/main/java/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PRINCE
+ */
+public class NewClass {
+     
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
+    
+
